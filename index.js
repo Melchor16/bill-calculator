f29998921ab2a8cbfdb09aba7ff1c299bc77895d
Melchor16 <erick.vimel@hotmail.com>
2023-04-20 16:32:51 -0600

    project finiched

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ $custom_tip = d.getElementById('custom_tip'),
 $ppl = d.getElementById('people'),
 $error = d.getElementById('error'),
 $total_tip = d.getElementById('total_tip'),
-$total_bill = d.getElementById('total_bill');
+$total_bill = d.getElementById('total_bill'),
+$reset = d.getElementById('reset');
 
 let bill = 0,
 tip_percentage = 0,
@@ -27,48 +28,54 @@ function errorOff(){
 
 //-----RESULT FUNCTION-----//
 function result(){
+    $reset.classList.remove('reset_inactive');
     total_tip = (bill * tip_percentage)/ppl;
     total_bill = (bill/ppl) + total_tip;
 
-    $total_tip.innerHTML=total_tip;
-    $total_bill.innerHTML=total_bill;
+    $total_tip.innerHTML=`$${total_tip.toFixed(2)}`;
+    $total_bill.innerHTML=`$${total_bill.toFixed(2)}`;
 }
 
 //-----MAIN PROGRAM-----//
 for(let inputs of $radio_inputs){
     inputs.onclick=e=>{
-        $custom_tip.value=''
+        $reset.classList.remove('reset_inactive');
+        tip_percentage = Number(inputs.value);
+        $custom_tip.value=''    //erase the custom tip when radio is selected
         if($ppl.value==='' || $ppl.value === '0'){
             errorOn();
         }else{
             errorOff();
-            tip_percentage = Number(inputs.value);
             result();
         }
     }
 }
 
 $bill.addEventListener('change', e=>{
+    $reset.classList.remove('reset_inactive');
+    bill = Number($bill.value);
     if($ppl.value==='' || $ppl.value === '0'){
         errorOn();
     }else{
         errorOff();
-        bill = Number($bill.value);
         result();
     }
 })
 
 $ppl.addEventListener('change', e=>{
+    $reset.classList.remove('reset_inactive');
+    ppl = Number($ppl.value);
     if($ppl.value==='' || $ppl.value === '0'){
         errorOn();
     }else{
         errorOff();
-        ppl = Number($ppl.value);
         result();
     }
 })
 
 $custom_tip.addEventListener('change', e=>{
+    $reset.classList.remove('reset_inactive');
+    tip_percentage = Number($custom_tip.value/100);
     for(let inputs2 of $radio_inputs){  //uncheck the radio inputs if you put a custom tip
         inputs2.checked = false;
     }
@@ -77,8 +84,23 @@ $custom_tip.addEventListener('change', e=>{
         errorOn();
     }else{
         errorOff();
-        tip_percentage = Number($custom_tip.value);
         result();
     }
+})
+
+//-----RESET-----//
+
+$reset.addEventListener('click', e =>{
+    $bill.value=''
+    $custom_tip.value=''
+    $ppl.value=''
+    $total_bill.innerHTML='$0.00'
+    $total_tip.innerHTML='$0.00'
+
+    for(let inputs2 of $radio_inputs){
+        inputs2.checked = false;
+    }
+
+    $reset.classList.add('reset_inactive')
 })
 
